caa1263d701b3e99179c3f22d1c22567d9ea209f
Ivan Kocienski <ivan.kocienski@gmail.com>
2021-01-29 18:30:35 +0000

    Aspirations link to their associated todo lists

--- spec/views/aspirations/show_spec.rb
+++ spec/views/aspirations/show_spec.rb
@@ -1,13 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'aspirations/show.html.erb', type: :view do
-    context 'title' do
-        let(:aspiration) { FactoryBot.create(:aspiration) }
+    let(:aspiration) { FactoryBot.create(:aspiration) }
 
+    context 'title' do
         it 'is present' do
             assign :aspiration, aspiration
             render
             expect(render).to have_selector('h1', text: 'Learning German')
+        end
+    end
+
+    context 'aspiration with associated todo lists' do
+        it 'shows list of links' do
+            %i[todo_list todo_list_2 todo_list_3].each do |id|
+                FactoryBot.create id, aspiration: aspiration
+            end
+
+            assign :aspiration, aspiration
+            render
+            expect(render).to have_selector('ul#todo-lists li', count: 3)
         end
     end
 end
